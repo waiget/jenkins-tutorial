dbfe1e37e92a085541885e5212c28224b314e36c
Law <WLAW200@caledonian.ac.uk>
2019-10-15 16:23:06 +0100

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
+	System.out.println("ohaiyou gozaimasu");
         System.out.println("Test Line 1"); 
         System.out.println("Test Line 2"); 
         System.out.println("Test Line 3"); 
